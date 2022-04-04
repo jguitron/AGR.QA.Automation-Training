843cf5951eb20d7e20e2f158c3755a43b5b32ed8
Max-K <MKayumov@Ashleyfurniture.com>
2022-04-04 12:30:02 +0000

    Updated CukesRunner.java

--- src/test/java/com/apollo_automation/runners/CukesRunner.java
+++ src/test/java/com/apollo_automation/runners/CukesRunner.java
@@ -15,7 +15,7 @@ import org.junit.runner.RunWith;
         features="src/test/resources/features",
         glue= "com/apollo_automation/step_definitions",
         dryRun=false,
-        tags="@mobile"
+        tags="@desktop"
 )
 
 public class CukesRunner {
