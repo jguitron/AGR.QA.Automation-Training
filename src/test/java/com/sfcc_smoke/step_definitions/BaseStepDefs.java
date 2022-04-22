e2db06f97056ee49d340ed8b7f640f67a99bb5da
jguitron <jguitron@ashleyfurniture.com>
2022-04-22 15:21:42 -0400

    -Base Page refactored from common page

--- src/test/java/com/sfcc_smoke/step_definitions/BaseStepDefs.java
+++ src/test/java/com/sfcc_smoke/step_definitions/BaseStepDefs.java
@@ -1,48 +1,51 @@
 package com.sfcc_smoke.step_definitions;
 
-import com.sfcc_smoke.pages.CommonPage;
+import com.sfcc_smoke.pages.BasePage;
+import com.sfcc_smoke.pages.SearchPage;
 import com.sfcc_smoke.utilities.BrowserUtils;
 import com.sfcc_smoke.utilities.Driver;
+import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import org.junit.Assert;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import java.time.Duration;
 
-public class CommonPageDefs {
+public class BaseStepDefs {
 
     WebDriver driver = Driver.getDriver();
-    CommonPage commonPage = new CommonPage();
+    BasePage basePage = new BasePage();
+    SearchPage searchPage = new SearchPage();
 
     @When("user finds closest store by {string}")
     public void user_set_the_closet_store_by(String zipcode) {
-        BrowserUtils.waitForVisibility(commonPage.Storedownarrow, Duration.ofSeconds(5));
-        commonPage.HomeStoreZipcodeBox.sendKeys(zipcode + Keys.ENTER);
+        BrowserUtils.waitForVisibility(basePage.Storedownarrow, Duration.ofSeconds(5));
+        basePage.HomeStoreZipcodeBox.sendKeys(zipcode + Keys.ENTER);
     }
 
     @When("User searches for SKU {string} and clicks on it")
     public void serachitem(String Item) {
-        BrowserUtils.waitForVisibility(commonPage.searchbar, Duration.ofSeconds(5));
-        commonPage.searchbar.sendKeys(Item + Keys.ENTER);
+        BrowserUtils.waitForVisibility(basePage.searchbar, Duration.ofSeconds(5));
+        basePage.searchbar.sendKeys(Item + Keys.ENTER);
     }
 
     @When("User search for a SKU {string} and clicks on item 1 in result set")
     public void serachitem_result1(String Item) {
-        BrowserUtils.waitForVisibility(commonPage.searchbar, Duration.ofSeconds(5));
-        commonPage.searchbar.sendKeys(Item);
-        commonPage.searchbar_result1.click();
+        BrowserUtils.waitForVisibility(basePage.searchbar, Duration.ofSeconds(5));
+        basePage.searchbar.sendKeys(Item);
+        basePage.searchbar_result1.click();
     }
 
     @When("User navigate to cart page")
     public void clickonminicart() {
-        BrowserUtils.waitForVisibility(commonPage.minicarticon, Duration.ofSeconds(5));
-        commonPage.minicarticon.click();
+        BrowserUtils.waitForVisibility(basePage.minicarticon, Duration.ofSeconds(5));
+        basePage.minicarticon.click();
     }
 
     @When("User navigate back to cart page")
     public void clickonbacktocart() {
-        BrowserUtils.waitForVisibility(commonPage.bacttoCartIcon, Duration.ofSeconds(5));
-        commonPage.bacttoCartIcon.click();
+        BrowserUtils.waitForVisibility(basePage.bacttoCartIcon, Duration.ofSeconds(5));
+        basePage.bacttoCartIcon.click();
     }
 
     @When("User verifies that Paypal login page is launched")
@@ -51,4 +54,10 @@ public class CommonPageDefs {
         System.out.println(driver.getTitle());
 
     }
+
+    @Then("User clicks search icon")
+    public void user_clicks_search_icon() {
+        searchPage.searchIcon.click();
+    }
+
 }
