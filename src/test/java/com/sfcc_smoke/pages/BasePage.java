89a9aa0547926c96b4a006bbbb6e64d15dc0e62c
jguitron <jguitron@ashleyfurniture.com>
2022-04-22 15:21:42 -0400

    -Base Page refactored from common page

--- src/test/java/com/sfcc_smoke/pages/BasePage.java
+++ src/test/java/com/sfcc_smoke/pages/BasePage.java
@@ -5,9 +5,9 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
-public class CommonPage {
+public class BasePage {
 
-    public CommonPage(){
+    public BasePage(){
         PageFactory.initElements(Driver.getDriver(),this);
     }
 
