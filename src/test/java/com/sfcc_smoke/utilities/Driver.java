5a1f4ed17ce134cb0a9bea23cc837f1b6fae3621
Max-K <MKayumov@Ashleyfurniture.com>
2022-08-29 15:58:34 +0000

    Updated Driver.java

--- src/test/java/com/sfcc_smoke/utilities/Driver.java
+++ src/test/java/com/sfcc_smoke/utilities/Driver.java
@@ -142,10 +142,9 @@ public class Driver {
                             chromeOptionsMap.put("seleniumVersion", ConfigReader.getProperty("BS_SELENIUM_VERSION"));
                             chromeOptionsMap.put("video", "true");
 //                            chromeOptionsMap.put("geoLocation", "US");
-
                             bsChromeCaps.setCapability("bstack:options", chromeOptionsMap);
                             bsChromeCaps.setCapability("BS_SESSION_NAME", ConfigReader.getProperty("BS_SESSION_NAME"));
-                            bsChromeCaps.setCapability("buildName", ConfigReader.getProperty("BS_BUILD_NAME"));
+                            bsChromeCaps.setCapability("build", ConfigReader.getProperty("BS_BUILD_NAME"));
                             driverPool.set(new RemoteWebDriver(new URL(REMOTE_URL), bsChromeCaps.merge(chromeOptions.addArguments("--disable-notifications"))));
                             driverPool.get().manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
                         } catch (Exception e) {
